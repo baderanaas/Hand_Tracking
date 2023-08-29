838190a6ce7416a899504be044d33be28baaa9a6
baderanaas <anas.bader@insat.ucar.tn>
2023-08-29 12:49:00 +0100

    Hand Drawing

--- HandTracking.py
+++ HandTracking.py
@@ -6,12 +6,18 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+# hand drawing
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    print(results.multi_hand_landmarks)
+    # print(results.multi_hand_landmarks)
+    
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
     
     cv2.imshow("Image", img)
     key = cv2.waitKey(1)
