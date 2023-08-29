7fe0382a938e54f8437a8582266a4a0768762d3b
baderanaas <anas.bader@insat.ucar.tn>
2023-08-29 12:32:44 +0100

    Activating webcam

---
+++ HandTracking.py
@@ -0,0 +1,15 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
