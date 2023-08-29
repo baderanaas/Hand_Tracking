e12069530aa246d72155f0fabc172d80458247c9
baderanaas <anas.bader@insat.ucar.tn>
2023-08-29 13:49:59 +0100

    Finalizing the module

--- HandTracking.py
+++ HandTracking.py
@@ -1,6 +1,6 @@
 import cv2
 import mediapipe as mp
-import time
+# import time
 
 
 class HandDetector:
@@ -45,32 +45,32 @@ class HandDetector:
         return lmList
 
 
-def main():
-    pTime = 0
-    cTime = 0
+# def main():
+    # pTime = 0
+    # cTime = 0
 
-    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture(0)
 
-    detector = HandDetector()
+    # detector = HandDetector()
 
-    while True:
-        success, img = cap.read()
-        img = detector.findHands(img)
-        lmList = detector.findPosition(img)
-        if len(lmList) != 0:
-            print(lmList[4])
+    # while True:
+    #     success, img = cap.read()
+    #     img = detector.findHands(img)
+    #     lmList = detector.findPosition(img)
+    #     if len(lmList) != 0:
+    #         print(lmList[4])
 
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
+    #     cTime = time.time()
+    #     fps = 1 / (cTime - pTime)
+    #     pTime = cTime
 
-        cv2.putText(
-            img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3
-        )
+    #     cv2.putText(
+    #         img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3
+    #     )
 
-        cv2.imshow("Image", img)
-        key = cv2.waitKey(1)
+    #     cv2.imshow("Image", img)
+    #     key = cv2.waitKey(1)
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
